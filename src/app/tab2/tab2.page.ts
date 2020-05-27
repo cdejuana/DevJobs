409b995777a3ae08c28a8f9615aaaf304194a8ba
GuishermoCaballero <65781751+GuishermoCaballero@users.noreply.github.com>
2020-05-27 17:41:11 +0200

    mis ofertas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,101 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  private ofertas=[
+    {
+      id: "1",
+      id_empresa:"HakunaMatataVegie",
+      id_localidad:"Villaviciosa de Odon",
+      id_sector:"Cocina",
+      id_contrato:"Indefinido",
+      id_jornada:"Completa",
+
+      titulo: "Cocinero para restaurante vegano",
+      descripcion: "",
+      que_ofrece:"",
+      salario:"24.000 - 26.000",
+      fecha_alta:"12-05-2020",
+      fecha_limite_inscripcion:"30-6-2020",
+    },
+    {
+      id: "2",
+      id_empresa:"Goiko Grill",
+      id_localidad:"Madrid",
+      id_sector:"Cocina",
+      id_contrato:"Indefinido",
+      id_jornada:"Completa",
+
+      titulo: "Repartidor",
+      descripcion: "",
+      que_ofrece:"",
+      salario:"24.000 - 26.000",
+      fecha_alta:"12-05-2020",
+      fecha_limite_inscripcion:"30-6-2020",
+    },
+    {
+      id: "3",
+      id_empresa:"Telepizza",
+      id_localidad:"Barcelona",
+      id_sector:"Cocina",
+      id_contrato:"Indefinido",
+      id_jornada:"Completa",
+
+      titulo: "Jefe de cocina",
+      descripcion: "",
+      que_ofrece:"",
+      salario:"24.000 - 26.000",
+      fecha_alta:"12-05-2020",
+      fecha_limite_inscripcion:"30-6-2020",
+    },
+    {
+      id: "4",
+      id_empresa:"HakunaMatataVegie",
+      id_localidad:"Villaviciosa de Odon",
+      id_sector:"Cocina",
+      id_contrato:"Indefinido",
+      id_jornada:"Completa",
+
+      titulo: "Cocinero para restaurante vegano",
+      descripcion: "",
+      que_ofrece:"",
+      salario:"24.000 - 26.000",
+      fecha_alta:"12-05-2020",
+      fecha_limite_inscripcion:"30-6-2020",
+    },
+    {
+      id: "5",
+      id_empresa:"Goiko Grill",
+      id_localidad:"Madrid",
+      id_sector:"Cocina",
+      id_contrato:"Indefinido",
+      id_jornada:"Completa",
+
+      titulo: "Repartidor",
+      descripcion: "",
+      que_ofrece:"",
+      salario:"24.000 - 26.000",
+      fecha_alta:"12-05-2020",
+      fecha_limite_inscripcion:"30-6-2020",
+    },
+    {
+      id: "6",
+      id_empresa:"Telepizza",
+      id_localidad:"Barcelona",
+      id_sector:"Cocina",
+      id_contrato:"Indefinido",
+      id_jornada:"Completa",
+
+      titulo: "Jefe de cocina",
+      descripcion: "",
+      que_ofrece:"",
+      salario:"24.000 - 26.000",
+      fecha_alta:"12-05-2020",
+      fecha_limite_inscripcion:"30-6-2020",
+    },
+    
+  ]
+
+
   constructor() {}
 
 }
