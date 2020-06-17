a91416a9aee93b4c8de70e39ed70d5319fa8c086
Carlos de Juana <carlosdejuanajimenez@gmail.com>
2020-06-17 13:42:53 +0200

    getOfertas() return http.get en servicio ofertas

--- src/app/servicios/ofertas.service.ts
+++ src/app/servicios/ofertas.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+// import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfertasService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getOfertas(){
+    return this.http.get("https://reqres.in/api/users?page=2");
+  }
 }
