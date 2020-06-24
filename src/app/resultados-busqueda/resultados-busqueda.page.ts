d17c6fffbcd74f316a8c82a198688a744ffcb02f
Carlos de Juana <carlosdejuanajimenez@gmail.com>
2020-06-24 11:13:56 +0200

    infinite scroll no funciona

--- src/app/resultados-busqueda/resultados-busqueda.page.ts
+++ src/app/resultados-busqueda/resultados-busqueda.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService} from '../servicios/ofertas.service';
 import { Oferta } from './../oferta';
+import {InfiniteScrollModule} from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-resultados-busqueda',
@@ -15,29 +16,40 @@ export class ResultadosBusquedaPage implements OnInit {
   private contadorMostradas: number;
   private maximoMostradas: number;
 
-  constructor(public ofertas: OfertasService) { }
+  constructor(public ofertas: OfertasService, public infiniteScroll: InfiniteScrollModule) { }
 
-  ngOnInit() {
-    // this.maximoMostradas = Math.round(this.ofertas.listadoOfertas.length / 10)
-    this.maximoMostradas = this.ofertas.listadoOfertas.length;
-    this.listaOfertas = this.ofertas.listadoOfertas;    
+  ngOnInit() {    
+    this.listaOfertas = this.ofertas.listadoOfertas;
+    this.maximoMostradas = this.listaOfertas.length;
 
     if(this.ofertas.listadoOfertas.length > 10){      
       this.ofertasMostradas = this.listaOfertas.slice(0, 10);
     } else {
-      this.listaOfertas = this.ofertas.listadoOfertas;
+      this.ofertasMostradas = this.listaOfertas;
     }
 
     this.contadorMostradas = this.ofertasMostradas.length;
   }
 
-  // public aniadirOfertas() {
-  //   this.contadorMostradas = this.ofertasMostradas.length;
-  //   this.ofertasSiguientes = this.listaOfertas.slice(this.contadorMostradas, this.contadorMostradas + 10);
-  //   for (let index = 0; index < this.ofertasSiguientes.length; index++) {
-  //     //const element = array[index];
-      
-  //   }
-  // }
+  private aniadirOfertas() {  
+    let limiteSiguientes = 0;
+    if ((this.contadorMostradas + 10) > this.listaOfertas.length) {
+      limiteSiguientes = this.listaOfertas.length - this.contadorMostradas;
+    } else {
+      limiteSiguientes = this.contadorMostradas + 10;
+    }
+    this.ofertasSiguientes = this.listaOfertas.slice(this.contadorMostradas, limiteSiguientes);
+    for (let index = 0; index < this.ofertasSiguientes.length; index++) {
+      this.ofertasMostradas.push(this.ofertasSiguientes[index]);
+    }
+    this.contadorMostradas = this.ofertasMostradas.length;
+  }
+
+  public onScroll() {
+    console.log("scrolled!!");
+    if (this.contadorMostradas < this.maximoMostradas) {
+      this.aniadirOfertas();
+    }
+  }
 
 }
